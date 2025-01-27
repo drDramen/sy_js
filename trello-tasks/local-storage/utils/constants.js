abb4664de13ac3d9789250966b1b4520d72b324b
Dramen <vvpalchin@gmail.com>
2025-01-27 18:40:15 +0200

    chore: create constants

---
+++ trello-tasks/local-storage/utils/constants.js
@@ -0,0 +1,9 @@
+const ONE_DAY = 86400e3;
+const STORAGE_TYPE = {
+  LocalStorage: 'localStorage',
+  SessionStorage: 'sessionStorage',
+  Cookie: 'cookies',
+};
+const STORAGE_NAME = 'app-config';
+
+export { ONE_DAY, STORAGE_NAME, STORAGE_TYPE };
