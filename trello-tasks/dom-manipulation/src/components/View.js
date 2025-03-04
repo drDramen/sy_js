c15f64dacf1d7652e22f24893e67dd61e50494ed
Dramen <vvpalchin@gmail.com>
2025-03-04 18:16:23 +0200

    chore: move common components to src

---
+++ trello-tasks/dom-manipulation/src/components/View.js
@@ -0,0 +1,64 @@
+export class View {
+  constructor({ tag = 'div', props = {}, parentNode }) {
+    this.node = document.createElement(tag);
+    this.children = [];
+
+    if (props) {
+      Object.assign(this.node, props);
+    }
+
+    if (parentNode) {
+      parentNode.append(this.node);
+    }
+  }
+
+  append(...rest) {
+    rest.forEach((child) => {
+      if (child instanceof View) {
+        this.children.push(child);
+        this.node.append(child.node);
+      } else {
+        this.node.append(child);
+      }
+    });
+  }
+
+  addClass(...className) {
+    this.node.classList.add(...className);
+  }
+
+  removeClass(...className) {
+    this.node.classList.remove(...className);
+  }
+
+  toggleClass(className) {
+    this.node.classList.toggle(className);
+  }
+
+  getAttribute(attribute) {
+    this.node.getAttribute(attribute);
+  }
+
+  setAttribute(attribute, value) {
+    this.node.setAttribute(attribute, value);
+  }
+
+  removeAttribute(attribute) {
+    this.node.removeAttribute(attribute);
+  }
+
+  setContent(content) {
+    this.node.textContent = content;
+  }
+
+  remove() {
+    this.node.remove();
+  }
+
+  destroy() {
+    this.children.forEach((child) => {
+      child.destroy();
+    });
+    this.remove();
+  }
+}
