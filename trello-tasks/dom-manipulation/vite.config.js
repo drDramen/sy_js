018b2a89db952b2690cc619b8d5de12413f4ff93
Dramen <vvpalchin@gmail.com>
2025-03-05 12:17:36 +0200

    refactor: update

--- trello-tasks/dom-manipulation/vite.config.js
+++ trello-tasks/dom-manipulation/vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  server: {
+    port: 3000,
+    open: true,
+  },
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
